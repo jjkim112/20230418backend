482dfa297c0079195e26c2a95f47512ba707c0d7
jjkim112 <jjkim112@naver.com>
2023-04-18 18:43:16 +0900

    backendchatadd

--- 2/backend/app.js
+++ 2/backend/app.js
@@ -14,9 +14,15 @@ app.get("/", (req, res) => {
   res.send("Hello,Express!");
 });
 
+let messages = [];
+
 app.post("/chat", async (req, res) => {
   try {
     const { content } = req.body;
+
+    const NewMesaages = { role: "user", content: content };
+
+    messages = [...messages, NewMesaages];
 
     const bearerToken = req.headers.authorization?.substring(7);
     if (bearerToken !== process.env.SECRET_KEY) {
@@ -33,7 +39,7 @@ app.post("/chat", async (req, res) => {
       "https://api.openai.com/v1/chat/completions",
       {
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: content }],
+        messages: messages,
       },
       {
         headers: {
@@ -42,9 +48,14 @@ app.post("/chat", async (req, res) => {
         },
       }
     );
-
+    const answer = response.data.choices[0].message;
+    messages = [...messages, answer];
     // console.log(response.data.choices[0].message.content);
-    res.json({ ok: true, result: response.data.choices[0].message.content });
+    // console.log(response);
+    // res.json({ ok: true, result: response.data.choices[0].message.content });
+    // console.log(messages);
+    // console.log(messages[messages.length - 1].content);
+    res.json({ ok: true, result: messages[messages.length - 1].content });
   } catch (error) {
     console.error();
   }
